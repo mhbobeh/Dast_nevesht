abc0e0e6a129fe4903269516e48e3644ce6e3e94
mhbob <mhbobmack@gmail.com>
2022-05-27 14:29:30 +0430

    create blog/models.py

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,40 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
-# Create your models here.
+
+class Author(models.Model):
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    is_active = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=300)
+    content = models.TextField()
+    image = models.ImageField(upload_to='images/%Y/%m/%d', blank=True)
+    slug = models.SlugField(max_length=30,default=None, unique=True, blank=True)
+    author = models.ForeignKey(Author, on_delete=models.DO_NOTHING)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    is_active = models.BooleanField(default=True)
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    user = models.CharField(max_length=200)
+    text = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_approved = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.user
